33985401b9d9735eafc702ad8a47563f6a896503
mapdavid <qiang9425_234@163.com>
2019-12-09 22:14:35 +0800

    Site updated: 2019-12-09 22:14:31

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -1,14 +1,14 @@
-<!--浏览器搞笑标题-->
+//浏览器搞笑标题
  var OriginTitle = document.title;
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         //$('[rel="icon"]').attr('href', "/img/trhx.png");
          document.title = 'ヽ(●-`Д´-)ノ你丑你就走！';
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         //$('[rel="icon"]').attr('href', "/img/trhx.png");
          document.title = 'ヾ(Ő∀Ő3)ノ你帅就回来！' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
